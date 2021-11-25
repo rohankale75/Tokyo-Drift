3381fe3d49aebc51922e16dfdf6f48f73e8c3b1e
rohankale75 <89684095+rohankale75@users.noreply.github.com>
2021-11-25 12:23:30 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,19 @@
+
+public class String2 {
+
+	public static void main(String[] args) {
+		
+		String s = new String("Pune");
+		
+		String s1 = new String("Pune");
+		
+		String s2 = "Velocity";
+		String s3 = "API";
+		
+		System.out.println(s.equals(s1));
+		
+		System.out.println(s2==s3);
+		
+	}
+	
+}
