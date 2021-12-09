f1024312475171dd64fe6e9a7410c8fc9734c074
unknown <rohankale75@gmail.com>
2021-12-09 22:16:29 +0530

    modified Excel sheet

--- ExcelReader.java
+++ ExcelReader.java
@@ -3,8 +3,10 @@ package Selenium;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
@@ -14,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ExcelReader {
 	
-	public static  String readData(int row,int col) throws IOException {
+	public static  String readData(int row,int col,int sheets) throws IOException {
 		
 		String path=System.getProperty("user.dir")+"\\TestData2.xlsx";		
 		
@@ -22,43 +24,72 @@ public class ExcelReader {
 		
 		FileInputStream fis=new FileInputStream(file);
 		
-		
+		//Apache poi
 		XSSFWorkbook wb=new XSSFWorkbook(fis);
 		
-		XSSFSheet sheet=wb.getSheetAt(0);
+		XSSFSheet sheet=wb.getSheetAt(sheets);
 		
-		String value=sheet.getRow(row).getCell(col).getStringCellValue();
+		int sheetValue=(int)sheet.getRow(row).getCell(col).getNumericCellValue();
+		
+		String value=String.valueOf(sheetValue);
+		
 		System.out.println(value);
 		return value;
 	
 	}
 	
+public static void writeData(int row,int col, String value) throws IOException, InvalidFormatException {
+		
+		String path=System.getProperty("user.dir")+"\\TestData3.xlsx";	
+		
+		File file =new File(path);
+		
+		FileInputStream fis=new FileInputStream(file);
+		
+		XSSFWorkbook wb=new XSSFWorkbook (fis);
+		
+		XSSFSheet sheet=wb.getSheetAt(0);
+		
+		sheet.createRow(row).createCell(col).setCellValue(value);
+		
+		FileOutputStream fos=new FileOutputStream(file);
+		
+		wb.write(fos);
+		
+		wb.close();
 
-	public static void main(String[] args) throws IOException {
+	}
+	
+	
+
+	public static void main(String[] args) throws IOException, InvalidFormatException {
 		
 		
+		  System.setProperty("webdriver.chrome.driver",
+		  "C:\\Users\\LENOVO\\Desktop\\Slenium Jar\\Chrome_96\\chromedriver.exe");
+		  
+		  //Step-1 //Browser open- by creating 
+		  WebDriver driver =new ChromeDriver();
+		  
+		  //Step-2 //maximize the window 
+		  driver.manage().window().maximize();
+		  
+		  //Step-3 //https://opensource-demo.orangehrmlive.com/
+		  driver.get("https://www.facebook.com/");
+		  
+		  //step-4 
+		  WebElement emailBox=driver.findElement(By.id("email"));
+		  emailBox.sendKeys(readData(5,0,0));
+		  
+		  //Step-5 
+		  WebElement password=driver.findElement(By.name("pass"));
+		  password.sendKeys(readData(5,1,0));
+		  
+		  String title=driver.getTitle(); System.out.println(title);
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\Slenium Jar\\Chrome_96\\chromedriver.exe");
+		writeData(0,0,title);
 		
-		//Step-1
-		//Browser open- by creating 
-		WebDriver driver =new ChromeDriver();
-		
-		//Step-2
-		//maximize the window
-		driver.manage().window().maximize();
-		
-		//Step-3
-		//https://opensource-demo.orangehrmlive.com/
-		driver.get("https://www.facebook.com/");
-		
-		//step-4
-		WebElement emailBox=driver.findElement(By.id("email"));
-		emailBox.sendKeys(readData(0,0));
-		
-		//Step-5
-		WebElement password=driver.findElement(By.name("pass"));
-		password.sendKeys(readData(0,1));
+		driver.close();
 		
 
 	}
